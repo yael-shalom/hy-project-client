23711e446d4707905252bd780f7457110f7174b1
yael-shalom <yael7147757@gmail.com>
2024-10-07 22:38:27 +0300

    move script tag to script file

---
+++ js/quizzes.js
@@ -0,0 +1,34 @@
+const getAllQuizzes = async () => {
+    try {
+        const res = await fetch("http://localhost:5000/quizzes");
+        const quizzes = await res.json();
+        console.log(quizzes);
+        const quizzesCon = document.querySelector("#quizzes");
+        for (const quiz of quizzes) {
+            let div = document.createElement("div");
+            div.classList.add("quiz");
+            div.dataset.id = quiz._id;
+            div.textContent = quiz.name;
+            div.addEventListener("click", (event) => {
+                window.open(`./quiz.html?id=${quiz._id}`, '_self');
+            });
+            quizzesCon.appendChild(div);
+        }
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+const getAllCategories = async () => {
+    const res = await fetch("http://localhost:5000/categories");
+    const categories = await res.json();
+    console.log(categories);
+    const categoryCon = document.querySelector("#categories");
+    let ul = document.createElement("ul");
+    for (const category of categories) {
+        let li = document.createElement("li");
+        li.textContent = category.name;
+        ul.appendChild(li);
+    }
+    categoryCon.appendChild(ul);
+};
