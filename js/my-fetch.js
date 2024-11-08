f672d4a5ad4c06ded85e27108964ed62c6f9fcdb
yael-shalom <yael7147757@gmail.com>
2024-11-08 03:02:45 +0200

    fix my-fetch headers

--- js/my-fetch.js
+++ js/my-fetch.js
@@ -1,9 +1,10 @@
-const myFetch = async (url, method = 'GET', options = {}) => {
+const myFetch = async (url, method = 'GET', options = { headers: {} }) => {
     try {
         const res = await fetch(url, {
             ...options,
             method,
             headers: {
+                ...options.headers,
                 'Authorization': `Bearer ${localStorage.getItem('token')}`
             }
         });
