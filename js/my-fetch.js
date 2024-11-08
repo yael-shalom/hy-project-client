c38a9d2560d1edd921eda0cf5c1f0bd38a2bcd14
yael-shalom <yael7147757@gmail.com>
2024-11-08 02:53:52 +0200

    add ok to my-fetch answer

--- js/my-fetch.js
+++ js/my-fetch.js
@@ -10,9 +10,9 @@ const myFetch = async (url, method = 'GET', options = {}) => {
         const data = await res.json();
 
         if (!res.ok)
-            throw new Error({ status: res.status, message: data.error.message });
+            throw new Error({ ok: res.ok, status: res.status, message: data.error.message });
 
-        return { status: res.statusCode, data };
+        return { ok: res.ok, status: res.status, data };
     } catch (error) {
         throw error;
     }
