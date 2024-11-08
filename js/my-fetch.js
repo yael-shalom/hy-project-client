64c6c55c046b05db576c9c8e8be0bac8b436bdbf
yael-shalom <yael7147757@gmail.com>
2024-11-08 02:04:12 +0200

    add my-fetch - fetch with token fn

---
+++ js/my-fetch.js
@@ -0,0 +1,18 @@
+const myFetch = async (method, url) => {
+    try {
+        const res = await fetch(url, {
+            method,
+            headers: {
+                'Authorization': `Bearer ${localStorage.getItem('token')}`
+            }
+        });
+        const data = await res.json();
+
+        if (!res.ok)
+            throw new Error({ status: res.status, message: data.error.message });
+
+        return { status: res.statusCode, data };
+    } catch (error) {
+        throw error;
+    }
+}
