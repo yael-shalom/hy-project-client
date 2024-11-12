eb813d3eb267ff9ae879e4dc9f560c2f45d36359
yael-shalom <yael7147757@gmail.com>
2024-11-12 17:09:48 +0200

    add profile presentation

--- js/quiz.js
+++ js/quiz.js
@@ -187,6 +187,45 @@ async function deleteQuiz() {
 //     }
 // }
 
+//#region add profile
+const profileWindow = document.getElementById('profileWindow');
+
+function openProfileWindow() {
+    profileWindow.style.display = 'block';
+}
+
+async function handleRemove() {
+    // קוד להסרת משתמש
+    const userId = "6732ad87b6562d5e3932f894";
+    try {
+        const res = await myFetch(`${baseURL}/users/${userId}`, 'DELETE');
+
+        if (res.ok) {
+            localStorage.removeItem('isLogin');
+            localStorage.removeItem('token');
+            localStorage.removeItem('userImage');
+            localStorage.removeItem('username');
+            console.log('user deleted successfully');
+            window.open('../pages/quizzes.html', '_self');
+        } else {
+            console.error('Error deleting user');
+        }
+    } catch (error) {
+        console.error('Error:', error);
+        alert('Failed to delete user.');
+    }
+    profileWindow.style.display = 'none'; // לסגור את החלונית לאחר הפעולה
+}
+
+function handleLogout() {
+    // קוד להתנתקות
+    localStorage.setItem('isLogin', false);
+    localStorage.removeItem('token');
+    localStorage.removeItem('userImage');
+    localStorage.removeItem('username');
+    profileWindow.style.display = 'none'; // לסגור את החלונית לאחר הפעולה
+}
+
 onload = () => {
     getQuizById(searchParams.get('id'));
     updateQuiz();
